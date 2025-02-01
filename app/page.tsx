2b7cdcec07de24b422723db9398b87acd11dc2f6
isiagi <isiagigeofrey0@gmail.com>
2025-02-01 14:12:20 +0300

    chore

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Carousel } from "@/components/Carousel";
+import { ServicesSection } from "@/components/ServicesSection";
 import { WhatsOn } from "@/components/WhatsOn";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main>
       <Carousel />
       <WhatsOn />
+      <ServicesSection />
     </main>
   );
 }
